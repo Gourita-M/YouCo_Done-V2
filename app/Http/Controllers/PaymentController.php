de26658984069ed3d5b92d30db932427691c9a08
Mouad Gourita <mouad.gourita@gmail.com>
2026-02-12 12:25:14 +0100

    fix: testing env exposed in Github with my information

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -5,13 +5,18 @@ namespace App\Http\Controllers;
 use App\Services\PaymentService;
 use Illuminate\Http\Request;
 use App\Models\Reservations;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class PaymentController extends Controller
 {
     
     public function index($id)
     {
-        $Reservation = Reservations::find($id);
+        $Reservation = DB::table('reservations')
+                    ->join('restaurants', 'reservations.restaurants_id', '=', 'restaurants.id')
+                    ->select('reservations.*','restaurants.name')
+                    ->where('reservations.id', $id);
         return View('Payment.index', Compact('Reservation'));
     }
 }
